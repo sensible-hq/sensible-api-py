3a52a557745e96b869c4749e6018c733d1435719
Horacio Peña <horape@gmail.com>
2023-12-07 17:48:59 -0300

    metadata

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     author='Sensible Technologies, Inc',
     author_email='hello@sensible.so',
     description='Python SDK for Sensible, the developer-first platform for extracting structured data from documents so that you can build document-automation features into your SaaS products',
-    long_description='Python SDK for Sensible, the developer-first platform for extracting structured data from documents so that you can build document-automation features into your SaaS products',
     packages=find_packages(),
     install_requires=['requests'],
     long_description=long_description,
