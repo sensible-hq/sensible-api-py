79e8fe8b4aaf634d8cbbed23de26a379649b7208
Horacio Peña <horape@gmail.com>
2023-12-07 17:47:36 -0300

    metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+# Get the long description from the project's README.rst file
+with open(os.path.join(project_root, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='sensibleapi',
-    version='0.0.4',
+    version='0.0.5',
     author='Sensible Technologies, Inc',
     author_email='hello@sensible.so',
     description='Python SDK for Sensible, the developer-first platform for extracting structured data from documents so that you can build document-automation features into your SaaS products',
     long_description='Python SDK for Sensible, the developer-first platform for extracting structured data from documents so that you can build document-automation features into your SaaS products',
     packages=find_packages(),
+    install_requires=['requests'],
+    long_description=long_description,
+    url="https://github.com/sensible-hq/sensible-api-py"
 )
 
